69a7a3b6c18153dbff48804a6ff4712f382810a8
wojtekkurylo <wkurylo88@gmail.com>
2017-08-26 18:48:08 +0200

    Topic: Explicit Intent && Bundle
    * Implemented Explicit Intent to start new Activity onClick;
    * Sending Extras and showing result as Toast Message on new Activity

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -16,11 +16,13 @@
 package com.example.android.sunshine;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -31,6 +33,7 @@ import android.widget.Toast;
 
 import com.example.android.sunshine.ForecastAdapter.ForecastAdapterOnClickHandler;
 import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.DetailActivity;
 import com.example.android.sunshine.utilities.NetworkUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
@@ -117,11 +120,15 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
     @Override
     public void onClick(String weatherForDay) {
         Context context = this;
-        // TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
-        // TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
-        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show();
+        // COMPLETED (1) Create a new Activity called DetailActivity using Android Studio's wizard
+        // COMPLETED (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
+        // COMPLETED (3) Remove the Toast and launch the DetailActivity using an explicit Intent
+        Intent newIntent = new Intent(MainActivity.this, DetailActivity.class);
+        newIntent.putExtra(Intent.EXTRA_TEXT,weatherForDay);
+        Log.d("main","Weather onClick: " + weatherForDay);
+        startActivity(newIntent);
+
+
     }
 
     /**
